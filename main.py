13cbdb0c9713047ae14c8a5bfac9e5c25b03315c
Quantum Stock Exchange and Blockchain <80061955+Night-Traders-Dev@users.noreply.github.com>
2024-11-28 23:50:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from adafruit_display_text import bitmap_label as label
 import adafruit_displayio_sh1107
 import board
 import busio
+import digitalio
 import displayio
-from displayio import FourWire
+import keypad
 import microcontroller
 import terminalio
 import time
@@ -13,6 +14,8 @@ OLED_CS = board.GP9
 OLED_CLK = board.GP10
 OLED_DIN = board.GP11
 OLED_RST = board.GP12
+OLED_KEY0 = board.GP15
+OLED_KEY1 = board.GP17
 
 WIDTH = 128
 HEIGHT = 64
@@ -34,9 +37,9 @@ def draw_text(init_text, x, y):
     text_area.y = y
     splash.append(text_area)
 
-# Function to update text
 def update_text(new_text, x, y):
-    splash.pop()  # Remove the current text label
+    while len(splash) > 1:
+        splash.pop()
     new_text_area = label.Label(terminalio.FONT, text=new_text, color=wht_hex, scale=1)
     new_text_area.x = x
     new_text_area.y = y
@@ -49,12 +52,11 @@ def get_cpu_stats():
     cputemp1 = microcontroller.cpus[1].temperature
     return f"CPU Stats\n{cpufreq0}/{cpufreq1} Mhz\n{cputemp0:,.2f}/{cputemp1:,.2f} C"
 
-
-#init display
+# Initialize display
 displayio.release_displays()
 
 spi_bus = busio.SPI(OLED_CLK, OLED_DIN)
-display_bus = FourWire(spi_bus, command=OLED_DC, chip_select=OLED_CS, reset=OLED_RST)
+display_bus = displayio.FourWire(spi_bus, command=OLED_DC, chip_select=OLED_CS, reset=OLED_RST)
 
 # Create the display object
 display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
@@ -63,11 +65,30 @@ display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIG
 splash = displayio.Group()
 display.root_group = splash
 
-
-# Display the initial text
 draw_bg()
 draw_text("Kraken Machine", x=10, y=20)
+
+last_update_time = time.monotonic()
+keys = keypad.Keys((OLED_KEY0, OLED_KEY1), value_when_pressed=False, pull=True)
 while True:
-    time.sleep(1)
-    cpudata = get_cpu_stats()
-    update_text(f"{cpudata}", x=10, y=20)
+    time.sleep(0.1)  # Polling interval
+    event = keys.events.get()
+    if event:
+        if hasattr(event, "key_number") and "pressed" in str(event):
+            if event.key_number == 0:
+                update_text("Rebooting...", x=10, y=20)
+                time.sleep(0.5)
+                microcontroller.reset()
+            elif event.key_number == 1:
+                if display.is_awake:
+                    display.sleep()
+                else:
+                    display.wake()
+    else:
+
+        # Update display periodically with CPU stats
+        current_time = time.monotonic()
+        if current_time - last_update_time >= 1:
+            cpudata = get_cpu_stats()
+            update_text(f"{cpudata}", x=10, y=20)
+            last_update_time = current_time
