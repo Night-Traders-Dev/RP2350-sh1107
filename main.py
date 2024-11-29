ff4c2e2368f02fd0665e7c25b01fb5274364c92b
Quantum Stock Exchange and Blockchain <80061955+Night-Traders-Dev@users.noreply.github.com>
2024-11-28 23:58:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from adafruit_display_text import bitmap_label as label
 import adafruit_displayio_sh1107
 import board
 import busio
-import digitalio
 import displayio
 import keypad
 import microcontroller
@@ -31,11 +30,13 @@ def draw_bg():
     bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
     splash.append(bg_sprite)
 
+
 def draw_text(init_text, x, y):
     text_area = label.Label(terminalio.FONT, text=init_text, color=wht_hex, scale=1)
     text_area.x = x
     text_area.y = y
     splash.append(text_area)
+
 
 def update_text(new_text, x, y):
     while len(splash) > 1:
@@ -45,12 +46,14 @@ def update_text(new_text, x, y):
     new_text_area.y = y
     splash.append(new_text_area)
 
+
 def get_cpu_stats():
     cpufreq0 = microcontroller.cpus[0].frequency / 1_000_000
     cpufreq1 = microcontroller.cpus[1].frequency / 1_000_000
     cputemp0 = microcontroller.cpus[0].temperature
     cputemp1 = microcontroller.cpus[1].temperature
     return f"CPU Stats\n{cpufreq0}/{cpufreq1} Mhz\n{cputemp0:,.2f}/{cputemp1:,.2f} C"
+
 
 # Initialize display
 displayio.release_displays()
@@ -70,13 +73,18 @@ draw_text("Kraken Machine", x=10, y=20)
 
 last_update_time = time.monotonic()
 keys = keypad.Keys((OLED_KEY0, OLED_KEY1), value_when_pressed=False, pull=True)
+reboot_message_active = False
+
 while True:
-    time.sleep(0.1)  # Polling interval
+    time.sleep(0.1)
+
+    # Handle key events
     event = keys.events.get()
     if event:
-        if hasattr(event, "key_number") and "pressed" in str(event):
+        if event.pressed:
             if event.key_number == 0:
                 update_text("Rebooting...", x=10, y=20)
+                reboot_message_active = True
                 time.sleep(0.5)
                 microcontroller.reset()
             elif event.key_number == 1:
@@ -84,11 +92,10 @@ while True:
                     display.sleep()
                 else:
                     display.wake()
-    else:
+                time.sleep(0.1)
 
-        # Update display periodically with CPU stats
-        current_time = time.monotonic()
-        if current_time - last_update_time >= 1:
-            cpudata = get_cpu_stats()
-            update_text(f"{cpudata}", x=10, y=20)
-            last_update_time = current_time
+    current_time = time.monotonic()
+    if current_time - last_update_time >= 1 and not reboot_message_active:
+        cpudata = get_cpu_stats()
+        update_text(f"{cpudata}", x=10, y=20)
+        last_update_time = current_time
